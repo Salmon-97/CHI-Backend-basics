3004815eb5a91bc08517d1214e37e1d8be6cc47d
Salmon Munirat <Modupeolasalmon@gmail.com>
2024-06-12 18:20:43 +0100

    commit message

---
+++ Basics/Javascript/Intro/script.js
@@ -0,0 +1,10 @@
+ // outputting data from javascript
+ alert('hello world');
+//  outputting data from a console
+console.log('This should go to the console');
+// interacting with the user with front
+prompt('what is your name', '');
+// javascript syntax/ variable declaration assignment
+// let name; variable declaration
+// name = 'name' variable assignment
+
