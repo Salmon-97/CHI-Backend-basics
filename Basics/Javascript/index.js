e47ab5ec32ba3eb2da3853f45d299384a2b3439e
Your Name <you@example.com>
2024-06-25 18:43:39 +0100

    commit message

---
+++ Basics/Javascript/index.js
@@ -0,0 +1,24 @@
+// Assighnment on 5 other string methods and Math.random and two other numbers methods
+Strings Examples
+// 1. single quote in double quote. e.g
+let n = "modupeola is a 'girl'" 
+// 2. double quote in single quote e.g
+let answer = 'she is "modupe"'
+// 3. Escape characters e.g
+//in double quote 
+let statement = "Never \"giveup\" even when things look tough"
+// 4. escape characters e.g
+// in single quote
+let word = 'Water is an \'important\' part of life'
+// 5. string and object e.g
+let x = "john"
+let z = new String ("ife")
+
+Number
+// Math.random() method returns a random floating point number up to 0 (inclusive) but not 1 (exclusive) e.g
+let num = Math.random();
+// exponentials
+let y = 123e5;  12300000
+// hexadecimal
+let v = 0xFF;
+
